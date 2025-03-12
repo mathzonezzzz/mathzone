21b6de91d7d56b4934db1f58e20bd2ed111aabaf
mathzonezzzz <f52663960@gmail.com>
2025-03-11 21:21:04 -0700

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,16 +174,16 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
+  "made by faran",
   "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
+  "buy sourstrips",
+  "hmu for fixes",
   "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
+  "Follow me on tiktok",
+  "if u get caught im not responsible do not play in class or when your not supposed to",
+  "imbored",
   "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "givememoney",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
